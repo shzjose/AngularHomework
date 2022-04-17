e3f4dc5f3099dcb77d08f07b9f2a892b2c654592
joseshz <jose.sanchez@sciodev.com>
2022-04-16 20:19:05 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+const[searchText, setSearchText] = useState("");
+const [playerData, setPlayerData] = useState<any>({});
+
+//LEAGUE OF LEGENDS API KEY
+const API_KEY ="RGAPI-023e996a-b383-4222-a53f-7d909f3432a6";
+
+function searchForPlayer(){
+    var APICall = "https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + searchText + "?api_key=" + API_KEY;
+    
+    axios.get(APICall).then(function (response){
+      //Success
+      setPlayerData(response.data);
+    }).catch(function (error){
+
+    });
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h5>LOL SERACHER: </h5>
+        <input type="text" onChange={e => setSearchText(e.target.value)}></input>
+        <button onClick={e => searchForPlayer()}>Search for player</button>
+      </div>
+      {JSON.stringify(playerData) != '{}' ?
+        <>
+          <p>{playerData.name}</p>
+          <img width="100" height="100" src={"http://ddragon.leagueoflegends.com/cdn/12.7.1/img/profileicon/" + playerData.profileIconId + ".png"}></img>
+        </>    
+        : <> <p>No player data</p> </>
+        
+      }
     </div>
   );
 }
